2b09e7a4f2d618f30c5915d4372265c38ad78729
Lim Cheng Yang <chengyang1017@gmail.com>
2026-09-02 21:20:42 +0800

    feat: support runner session update events

--- lib/features/runner/models/runner_event.dart
+++ lib/features/runner/models/runner_event.dart
@@ -3,18 +3,27 @@ import 'run_session.dart';
 enum RunnerEventType {
   status,
   log,
+  session,
 }
 
 class RunnerEvent {
   const RunnerEvent.status(this.status)
       : type = RunnerEventType.status,
-        message = null;
+        message = null,
+        session = null;
 
   const RunnerEvent.log(this.message)
       : type = RunnerEventType.log,
-        status = null;
+        status = null,
+        session = null;
+
+  const RunnerEvent.session(this.session)
+      : type = RunnerEventType.session,
+        status = null,
+        message = null;
 
   final RunnerEventType type;
   final RunnerStatus? status;
   final String? message;
+  final RunSession? session;
 }
